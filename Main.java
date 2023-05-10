6d00d3535d6e4304b2e18b2dff315334973ff740
Yeeshean <yeesheanlee@gmail.com>
2023-05-10 14:32:20 +0800

    Add Main and Rule

---
+++ Main.java
@@ -0,0 +1,37 @@
+package Assignment.clone;
+
+import java.util.Scanner;
+
+public class Main {
+    // Main method to run the game
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
+
+        while (isRunning) {
+            game.printBoard();
+            System.out.println("Enter row and column where you want to place your mark (e.g. 1 2): ");
+            int row = scanner.nextInt() - 1;
+            int col = scanner.nextInt() - 1;
+            if (game.placeMark(row, col)) {
+                if (game.checkForWin(row, col)) {
+                    game.printBoard();
+                    System.out.println("Congratulations, player " + game.currentPlayerMark + " has won!");
+                    isRunning = false;
+                }
+                else if (game.isBoardFull()) {
+                    game.printBoard();
+                    System.out.println("It's a tie!");
+                    isRunning = false;
+                }
+                else {
+                    game.changePlayer();
+                }
+            }
+            else {
+                System.out.println("Invalid move, please try again.");
+            }
+        }
+    }
+}
