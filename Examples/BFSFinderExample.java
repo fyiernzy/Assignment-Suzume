4141ce40891d6eb8822415f7053fa03f1aedec17
fyiernzy <fyiernzy@gmail.com>
2023-05-11 11:58:55 +0800

    ✨ Add examples as a simple guidance to use the class and method

---
+++ Examples/BFSFinderExample.java
@@ -0,0 +1,17 @@
+package Examples;
+
+import PixelMap.*;
+import PathUtils.Searching.*;
+import java.util.*;
+
+public class BFSFinderExample {
+    public static void main(String[] args) {
+        PixelImageReader reader = new PixelImageReader();
+        List<PixelMap> list = reader.readImages("Examples/Images", ".png");
+        list.stream().forEach(map -> map.transform(64));
+
+        PixelMap map1 = list.get(0);
+        BFSFinder finder = new BFSFinder(map1.getPixelMap());
+        System.out.println(finder.countPaths(3));
+    }
+}
---
+++ Examples/DFSFinderExample.java
@@ -0,0 +1,17 @@
+package Examples;
+
+import PixelMap.*;
+import PathUtils.Searching.*;
+import java.util.*;
+
+public class DFSFinderExample {
+    public static void main(String[] args) {
+        PixelImageReader reader = new PixelImageReader();
+        List<PixelMap> list = reader.readImages("Examples/Images", ".png");
+        list.stream().forEach(map -> map.transform(64));
+
+        PixelMap map1 = list.get(0);
+        DFSFinder finder = new DFSFinder(map1.getPixelMap());
+        System.out.println(finder.countPaths(3));
+    }
+}
