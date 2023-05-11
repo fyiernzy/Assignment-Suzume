7c5d4fed1210c91ba483913aa0f234b67148b54a
fyiernzy <fyiernzy@gmail.com>
2023-05-11 08:55:06 +0800

    ✨  Refactor the code

--- TicTacToe/ShortestPathFinder.java
+++ TicTacToe/ShortestPathFinder.java
@@ -45,79 +45,98 @@ public class ShortestPathFinder {
     private int[][] map;
     private int numRows;
     private int numCols;
+    private List<Cell> paths;
 
     ShortestPathFinder(int[][] map) {
-        this.map = map;
-        this.numRows = map.length;
-        this.numRows = map[0].length;
+        setMap(map);
     }
 
     ShortestPathFinder(PixelMap pixelMap) {
         this(pixelMap.getPixelMap());
     }
 
-    int heuristic(Cell n, Cell goal) {
+    public void setMap(int[][] map) {
+        this.map = map;
+        this.numRows = map.length;
+        this.numRows = map[0].length;
+    }
+
+    private int heuristic(Cell n, Cell goal) {
         return Math.abs(n.x - goal.x) + Math.abs(n.y - goal.y);
     }
 
-    boolean isValid(int row, int col) {
+    private boolean isValid(int row, int col) {
         return (row >= 0 && row < numRows)
                 && (col >= 0 && col < numCols)
                 && map[row][col] != 1;
     }
 
-    
-    List<Cell> findPath(int[][] grid, Cell start, Cell goal) {
+    private Cell[] initializeCells() {
+        Cell goal = new Cell(numRows - 1, numCols - 1, null, 0, 0);
+        Cell start = new Cell(0, 0, null, 0, heuristic(new Cell(0, 0, null, 0, 0), goal));
+        return new Cell[] { start, goal };
+    }
+
+    private List<Cell> getNeighbors(Cell cell) {
+        List<Cell> neighbors = new ArrayList<>();
+        int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } }; // directions
+        for (int[] dir : dirs) {
+            int x = cell.x + dir[0]; // neighbor's x coordinate
+            int y = cell.y + dir[1]; // neighbor's y coordinate
+
+            // Check if the neighbor is valid and not in the closed set
+            if (isValid(x, y)) {
+                neighbors.add(new Cell(x, y, null, 0, 0));
+            }
+        }
+        return neighbors;
+    }
+
+    private List<Cell> reconstructPath(Cell current) {
+        List<Cell> path = new ArrayList<>();
+        while (current != null) {
+            path.add(current);
+            current = current.parent;
+        }
+        Collections.reverse(path);
+        return path;
+    }
+
+    public List<Cell> findPath() {
+        Cell[] cells = initializeCells();
+        Cell start = cells[0], goal = cells[1];
         PriorityQueue<Cell> openSet = new PriorityQueue<>((a, b) -> a.f - b.f);
+        Set<Cell> closedSet = new HashSet<>();
 
-        // Create a hash set to store the closed set of cells
-        HashSet<Cell> closedSet = new HashSet<>();
-
-        // Add the start cell to the open set
         openSet.add(start);
 
-        // Loop until the open set is empty or the goal is reached
         while (!openSet.isEmpty()) {
-            // Get the cell with the lowest f(n) value from the open set and remove it
             Cell current = openSet.poll();
 
-            // Add it to the closed set
             closedSet.add(current);
 
-            // Check if it is the goal cell
             if (current.equals(goal)) {
-                // Return the path by tracing back the parent cells
-                List<Cell> path = new ArrayList<>();
-                while (current != null) {
-                    path.add(current);
-                    current = current.parent;
-                }
-                Collections.reverse(path); // reverse the order of the path
-                return path;
+                List<Cell> paths = reconstructPath(current);
+                this.paths = paths;
+                return paths;
             }
 
-            // Generate the neighbors of the current cell
-            int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } }; // up, down, left, right directions
-            for (int[] dir : dirs) {
-                int x = current.x + dir[0]; // neighbor's x coordinate
-                int y = current.y + dir[1]; // neighbor's y coordinate
-
-                // Check if the neighbor is valid and not in the closed set
-                if (isValid(grid, x, y) && !closedSet.contains(new Cell(x, y, null, 0, 0))) {
+            for (Cell neighbor : getNeighbors(current)) {
+                if (!closedSet.contains(neighbor)) {
                     // Calculate the g(n), h(n) and f(n) values for the neighbor
                     int g = current.g + 1; // assume unit cost for each step
-                    int h = heuristic(new Cell(x, y, null, 0, 0), goal);
+                    int h = heuristic(neighbor, goal);
                     int f = g + h;
 
                     // Check if the neighbor is in the open set and has a lower f(n) value
                     boolean better = openSet.stream()
-                            .filter(cell -> cell.equals(new Cell(x, y, null, 0, 0)))
+                            .filter(cell -> cell.equals(neighbor))
                             .allMatch(cell -> cell.f > f);
 
                     // If the neighbor is better, add it to the open set with the current cell as
                     // its parent
                     if (better) {
-                        openSet.add(new Cell(x, y, current, g, h));
+                        openSet.add(new Cell(neighbor.x, neighbor.y, current, g, h));
                     }
                 }
             }
@@ -125,6 +144,29 @@ public class ShortestPathFinder {
 
         // Return null if no path is found
         return null;
+    }
+    
+    public void printMap() {
+        printMap(this.map);
+    }
+
+    void printMap(int[][] map) {
+        for (int[] row : map) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printCoordinates() {
+        printCoordinates(this.paths);
+    }
+
+    void printCoordinates(List<Cell> path) {
+        for (Cell cell : path) {
+            System.out.println("(" + cell.x + ", " + cell.y + ")");
+        }
     }
 
     public static void main(String[] args) {
@@ -137,21 +179,17 @@ public class ShortestPathFinder {
         };
 
         // Define the start cell and the goal cell
-        Cell start = new Cell(0, 0, null, 0, heuristic(new Cell(0, 0, null, 0, 0), new Cell(4, 4, null, 0, 0)));
-        Cell goal = new Cell(4, 4, null, 0, 0);
-        // Find the shortest path using A* algorithm
-        List<Cell> path = findPath(grid, start, goal);
 
+        // Find the shortest path using A* algorithm
+        ShortestPathFinder finder = new ShortestPathFinder(grid);
+        List<Cell> path = finder.findPath();
+        finder.printMap();
         // Print the grid with the path marked as *
         // printPath(grid ,path);
 
         // Print the coordinates of each cell in the path
-        printCoordinates(path);
+        // finder.printCoordinates(path);
     }
 
-    public static void printCoordinates(List<Cell> path) {
-        for (Cell cell : path) {
-            System.out.println("(" + cell.x + ", " + cell.y + ")");
-        }
-    }
+    
 }
