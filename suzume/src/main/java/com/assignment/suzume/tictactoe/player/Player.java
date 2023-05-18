7921b15b8bc6586eb1f317b3407c571080f1330e
fyiernzy <fyiernzy@gmail.com>
2023-05-18 23:03:30 +0800

    🍀 Refactor player class for future extensibility

---
+++ suzume/src/main/java/com/assignment/suzume/tictactoe/player/Player.java
@@ -0,0 +1,7 @@
+package com.assignment.suzume.tictactoe.player;
+
+import com.assignment.suzume.tictactoe.board.GamingBoard;
+
+public abstract class Player {
+    public abstract void makeMove(GamingBoard board);
+}
