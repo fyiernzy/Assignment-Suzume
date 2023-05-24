3694600499519067992d1083b05e9063a022840d
Ying Shan <yingshan1209@gmail.com>
2023-05-24 23:38:24 +0800

    Update

--- suzume/src/main/java/com/assignment/suzume/mapper/GamerMapper.java
+++ suzume/src/main/java/com/assignment/suzume/mapper/GamerMapper.java
@@ -5,8 +5,6 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-@MapperScan("com.assignment.suzume.mapper")
 @Mapper
 public interface GamerMapper {
     void insertGamer(Gamer gamer);
