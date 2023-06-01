b1915dde1619eef45ab3fd59fabe5a80d2122768
fyiernzy <fyiernzy@gmail.com>
2023-06-01 14:59:55 +0800

    Implement JsonFileReader
    - Read the user data from json fiel
    - Allow DB-like structure

---
+++ suzume/src/main/java/com/assignment/suzume/connecting/account/JsonFileReader.java
@@ -0,0 +1,25 @@
+package com.assignment.suzume.connecting;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class JsonFileReader {
+    private JsonObject jsonObject;
+
+    private JsonFileReader(JsonObject jsonObject) {
+        this.jsonObject = jsonObject;
+    }
+
+    public static JsonFileReader createJsonFileReader(Reader reader) {
+        JsonReader jsonReader = Json.createReader(reader)
+        JsonObject jsonObject = jsonReader.readObject();
+        return new JsonFileReader(jsonObject);
+    }
+
+    public String getStringValue(String key) {
+        return jsonObject.getString(key);
+    }
+}
