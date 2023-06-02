a48950c855a3ee5217d5907dd4d457b225955854
fyiernzy <fyiernzy@gmail.com>
2023-06-02 22:40:30 +0800

    Remove unused 'SettingManager'

---
+++ suzume/src/main/java/com/assignment/suzume/connecting/account/LoginManager.java
@@ -0,0 +1,43 @@
+package com.assignment.suzume.connecting.account;
+
+import java.util.Scanner;
+import com.assignment.suzume.connecting.ConsolePrinter;
+
+public class LoginManager {
+    private Scanner scanner;
+    private SignInManager signInManager;
+    private SignUpManager signUpManager;
+
+    LoginManager() {
+        this.scanner = new Scanner(System.in);
+        this.signInManager = new SignInManager();
+        this.signUpManager = new SignUpManager();
+    }
+
+    public void login() {
+        ConsolePrinter.printBanner();
+        ConsolePrinter.printWelcomeMessage();
+
+        while(true) {
+            System.out.println(" --> [1] Sign in");
+            System.out.println(" --> [2] Sign up");
+            System.out.println(" --> [3] Exit");
+            
+            ConsolePrinter.printDecorator();
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(choice) {
+                case 1 -> signInManager.run();
+                case 2 -> signUpManager.run();
+                case 3 -> System.exit(0);
+                default -> System.out.println("Invalid choice.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        LoginManager loginHelper = new LoginManager();
+        loginHelper.login();
+    }
+}
