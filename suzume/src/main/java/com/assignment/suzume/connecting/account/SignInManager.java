85a46cd6591ddaef1477a20554fcd4874e343f9d
fyiernzy <fyiernzy@gmail.com>
2023-06-02 22:48:44 +0800

    Implement the  function when user login successfully

--- suzume/src/main/java/com/assignment/suzume/connecting/account/SignInManager.java
+++ suzume/src/main/java/com/assignment/suzume/connecting/account/SignInManager.java
@@ -1,6 +1,10 @@
 package com.assignment.suzume.connecting.account;
 
+import java.io.*;
 import java.util.Scanner;
+
+import com.assignment.suzume.connecting.configuration.Configuration;
+import com.google.gson.Gson;
 
 public class SignInManager {
     private Scanner scanner;
@@ -9,7 +13,7 @@ public class SignInManager {
         this.scanner = new Scanner(System.in);
     }
 
-    public boolean run() {
+    public void run() {
         String username, password;
 
         System.out.print("Enter your username: ");
@@ -19,14 +23,23 @@ public class SignInManager {
 
         if(!UserAuthenticationManager.checkIfUserExist(username)) {
             System.out.println("User does not exist!");
-            return false;
+            return ;
         }
 
         if(!UserAuthenticationManager.checkIfPasswordMatch(username, password)) {
             System.out.println("Password does not match!");
-            return false;
+            return ;
         }
 
-        return true;
+        Gson gson = new Gson();
+        
+        try (FileReader reader = new FileReader(new File(Configuration.GAME_FOLDER_URL, username + "/user.json"))) {
+            // Deserialize JSON file into MyClass object
+            User user  = gson.fromJson(reader, User.class);
+            Dashboard dashboard = new Dashboard(user);
+            dashboard.showDashboard();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
