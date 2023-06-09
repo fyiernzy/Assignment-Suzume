9317bd88700d477ad3fec4f7f9706bbee35aa26c
fyiernzy <fyiernzy@gmail.com>
2023-06-09 09:44:21 +0800

    💯 Modify the appearance of the selection panel

--- suzume/src/main/java/com/assignment/suzume/connecting/account/data/GameDataManager.java
+++ suzume/src/main/java/com/assignment/suzume/connecting/account/data/GameDataManager.java
@@ -1,9 +1,12 @@
 package com.assignment.suzume.connecting.account.data;
 
-import java.util.Properties;
 import java.io.*;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.util.Properties;
+import mdlaf.MaterialLookAndFeel;
+import mdlaf.utils.MaterialColors;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import com.assignment.suzume.connecting.configuration.Configuration;
 
 public class GameDataManager {
@@ -28,40 +31,58 @@ public class GameDataManager {
             configureGameFolderURL();
             saveConfiguration();
             Configuration.setGameFolderURL(gameFolderURL);
-        } 
+        }
 
-        if(gameFolderURL == null) {
+        if (gameFolderURL == null) {
             this.gameFolderURL = properties.getProperty("game_folder_url");
         }
-        
+
         createGameFolder();
     }
 
+    // ...
+
     private void configureGameFolderURL() {
-        // Create a JFrame to act as the parent for the file chooser dialog
-        JFrame parentFrame = new JFrame();
-
-        // Create an instance of JFileChooser
-        JFileChooser fileChooser = new JFileChooser();
-
-        // Set the file chooser to select directories only
-        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-
-        // Show the file chooser dialog
-        int result = fileChooser.showOpenDialog(parentFrame);
-
-        // Check if the user clicked the "Open" button
-        if (result == JFileChooser.APPROVE_OPTION) {
-            // Get the selected file
-            File selectedFolder = fileChooser.getSelectedFile();
-
-            // Get the absolute path of the selected folder
-            this.gameFolderURL = selectedFolder.getAbsolutePath() + "\\suzume\\";
+        // Set the Material Design look and feel
+        try {
+            UIManager.setLookAndFeel(new MaterialLookAndFeel());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
         }
+    
+        JFrame parentFrame = new JFrame();
+        File defaultDirectory = getDefaultDirectory();
+    
+        JFileChooser fileChooser = new JFileChooser(defaultDirectory);
+        fileChooser.updateUI();
+        fileChooser.setDialogTitle("Choose a folder to save your game data");
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+    
+        // Customize the appearance of the file chooser
+        UIManager.put("FileChooser.background", MaterialColors.CYAN_50);
+        UIManager.put("FileChooser.foreground", MaterialColors.BLACK);
+        UIManager.put("FileChooser.selectionBackground", MaterialColors.CYAN_100);
+        UIManager.put("FileChooser.selectionForeground", MaterialColors.WHITE);
+        UIManager.put("FileChooser.acceptButtonToolTipText", "Select");
+        UIManager.put("FileChooser.cancelButtonToolTipText", "Cancel");
+    
+        int result = fileChooser.showOpenDialog(parentFrame);
+    
+        File selectedFolder = (result == JFileChooser.APPROVE_OPTION)
+                ? fileChooser.getSelectedFile()
+                : defaultDirectory;
+    
+        this.gameFolderURL = selectedFolder.getAbsolutePath() + File.separator + "suzume" + File.separator;
+    }
+
+    private File getDefaultDirectory() {
+        String userHome = System.getProperty("user.home");
+        File programFileDir = new File(userHome);
+        return programFileDir;
     }
 
     public String getGameFolderURL() {
-        if(this.gameFolderURL == null) {
+        if (this.gameFolderURL == null) {
             properties.getProperty("game_folder_url");
         }
         return gameFolderURL;
