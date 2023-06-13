95eb9fa1608a01ebc0d7865758965f2a7a30df98
egsee <egsee@gmail.com>
2023-06-13 17:40:42 +0800

    Input Handler updated, make move bug fixed

--- suzume/src/main/java/com/assignment/suzume/connecting/account/InputHandler.java
+++ suzume/src/main/java/com/assignment/suzume/connecting/account/InputHandler.java
@@ -21,6 +21,34 @@ public class InputHandler {
         return input;
     }
 
+    public static void next(){
+        scanner.nextLine();
+    }
 
+    public static int[] readTwoIntegers() {
+        int[] numbers = new int[2];
+
+        boolean validInput = false;
+        while (!validInput) {
+            String input = scanner.nextLine();
+
+            String[] splitInput = input.trim().split("\\s+");
+            if (splitInput.length == 2) {
+                try {
+                    numbers[0] = Integer.parseInt(splitInput[0]);
+                    numbers[1] = Integer.parseInt(splitInput[1]);
+                    validInput = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input. Please enter two valid integers.");
+                    System.out.println("Enter your move (row column): ");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter two integers separated by a space.");
+                System.out.println("Enter your move (row column): ");
+            }
+        }
+
+        return numbers;
+    }
 
 }
--- suzume/src/main/java/com/assignment/suzume/tictactoe/player/Gamer.java
+++ suzume/src/main/java/com/assignment/suzume/tictactoe/player/Gamer.java
@@ -2,24 +2,22 @@ package com.assignment.suzume.tictactoe.player;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.util.*;
+
+import com.assignment.suzume.connecting.account.InputHandler;
 import com.assignment.suzume.tictactoe.board.GamingBoard;
 
 public class Gamer extends Player {
-     transient private Scanner scanner;
 
      public Gamer(String name, char mark) {
           super(name, mark);
-          this.scanner = new Scanner(System.in);
      }
 
      @Override
      public int[] makeMove(GamingBoard board) {
-          int[] move = new int[2];
+          int[] move;
           while (true) {
                System.out.println("Enter your move (row column): ");
-               move[0] = scanner.nextInt();
-               move[1] = scanner.nextInt();
+               move = InputHandler.readTwoIntegers();
                System.out.println("Your move is: " + move[0] + " " + move[1]);
 
                if (!board.isValidMove(move[0], move[1])) {
@@ -43,6 +41,5 @@ public class Gamer extends Player {
 
      private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
-        this.scanner = new Scanner(System.in);
     }
 }
