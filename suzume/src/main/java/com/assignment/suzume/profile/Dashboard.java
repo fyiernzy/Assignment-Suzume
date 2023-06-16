0109cb2a75f9f2236e12f9765a4853db21f5a764
fyiernzy <fyiernzy@gmail.com>
2023-06-17 00:52:34 +0800

    ✨ Minor amendment to adapt to the latest changes

--- suzume/src/main/java/com/assignment/suzume/profile/Dashboard.java
+++ suzume/src/main/java/com/assignment/suzume/profile/Dashboard.java
@@ -1,5 +1,6 @@
 package com.assignment.suzume.profile;
 
+import com.assignment.suzume.constants.FontStyle;
 import com.assignment.suzume.game.*;
 import com.assignment.suzume.data.*;
 import com.assignment.suzume.utils.*;
@@ -15,16 +16,31 @@ public class Dashboard {
 
     public void checkAccountAnalysis() {
         final int formatSize = 20;
-        String format = "%" + formatSize + "s : %s\n";
-        System.out.println("Account Analysis");
-        System.out.printf(format, "Name", user.getName());
-        System.out.printf(format, "Win", user.getWin());
-        System.out.printf(format, "Lose", user.getLose());
-        System.out.printf(format, "Draw", user.getDraw());
-        System.out.printf(format, "Win Rate", user.getWinRate() + "%");
-        System.out.printf(format, "Lose Rate", user.getLoseRate() + "%");
-        System.out.printf(format, "Draw Rate", user.getDrawRate() + "%");
-        System.out.printf(format, "Score", user.getScore());
+        String format = "\u001B[1;36m|" + padRight("\u001B[1;36m%-" + (formatSize - 1) + "s", formatSize)
+                + "\u001B[1;36m| %s|\n";
+        String border = "\u001B[1;36m+" + repeat("-", formatSize + 7) + "+------------------+\n"; // Border design
+
+        System.out.println("\033[1;93mAccount Analysis");
+        System.out.print(border);
+        System.out.printf(format, " Name", padRight(user.getName(), formatSize - 3));
+        System.out.print(border);
+        System.out.printf(format, " Win", padRight(Integer.toString(user.getWin()), formatSize - 3));
+        System.out.printf(format, " Lose", padRight(Integer.toString(user.getLose()), formatSize - 3));
+        System.out.printf(format, " Draw", padRight(Integer.toString(user.getDraw()), formatSize - 3));
+        System.out.printf(format, " Win Rate", padRight(user.getWinRate() + "%", formatSize - 3));
+        System.out.printf(format, " Lose Rate", padRight(user.getLoseRate() + "%", formatSize - 3));
+        System.out.printf(format, " Draw Rate", padRight(user.getDrawRate() + "%", formatSize - 3));
+        System.out.printf(format, " Score", padRight(Integer.toString(user.getScore()), formatSize - 3));
+        System.out.print(border);
+        System.out.println("\u001B[1;35m");
+    }
+
+    private static String repeat(String str, int times) {
+        return new String(new char[times]).replace('\0', str.charAt(0));
+    }
+
+    private static String padRight(String text, int size) {
+        return String.format("%-" + size + "s", text);
     }
 
     public void showDashboard() {
@@ -39,32 +55,28 @@ public class Dashboard {
             System.out.println(" --> [4] Show game replay");
             System.out.println(" --> [5] Logout");
 
-            ConsolePrinter.printDecorator();
-
-
             int choice = InputHandler.getIntInput();
-
 
             switch (choice) {
                 case 1 -> checkAccountAnalysis();
                 case 2 -> new ConsoleGame(MapUtils.getCombinedMap()).play();
                 case 3 -> DatabaseManager.getInstance().showDatabase();
                 case 4 -> {
-                    GamingBoard board = GameFileDataManager.getInstance().loadGameReplay();
-                    VideoPlayer player = VideoPlayer.getSuitableVideoPlayer(board);
-                    player.replay();
+                    GamingBoard board = GameFileInputHandler.getInstance().handleLoadReplay();
+                    if (board != null) {
+                        VideoPlayer player = VideoPlayer.getSuitableVideoPlayer((GamingBoard) board);
+                        player.replay();
+                    }
                 }
                 case 5 -> {
-                    System.out.println("Logging out...");
-                    for(int i = 0; i < 3; i++) {
-                        try {
-                            Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+                    System.out.println(FontStyle.BLUE + "Logging out...");
+                    for (int i = 0; i < 3; i++) {
+                        Timer.waitInMilliseconds(500);
                         System.out.print(".");
                     }
-                    System.out.println("\nLogout successfully.");
+
+                    Timer.waitInMilliseconds(500);
+                    System.out.println("\n" + FontStyle.PURPLE + "Logout successfully.\n");
                     break loop;
                 }
                 default -> System.out.println("Invalid choice.");
@@ -72,6 +84,5 @@ public class Dashboard {
         }
 
     }
-
 
 }
